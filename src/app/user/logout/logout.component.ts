f0761f613dcae585bb009652f25b9aae26c5de61
MagicKitty <nicolasblin585@gmail.com>
2021-06-27 12:19:14 +0200

    9min - logout spinner

--- src/app/user/logout/logout.component.ts
+++ src/app/user/logout/logout.component.ts
@@ -12,19 +12,25 @@ export class LogoutComponent implements OnInit {
   userServiceSubscription: Subscription;
   errorMessage: string;
 
+  loading: boolean;
+
   constructor(private userService: UserService, private router: Router) {
     this.userServiceSubscription = new Subscription();
     this.errorMessage = '';
+    this.loading = false;
   }
 
   ngOnInit(): void {}
 
   logout(): void {
+    this.loading = true;
     this.userService.logout().subscribe(
       (next) => {
+        this.loading = false;
         this.router.navigate(['/']);
       },
       (error) => {
+        this.loading = false;
         this.errorMessage = error;
       }
     );
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { UserRoutingModule } from './user-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -16,7 +17,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     UserRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ]
 })
 export class UserModule { }
