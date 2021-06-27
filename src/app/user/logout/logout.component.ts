4772cc28045b2ffaef35f8128be9c3e32c9bd962
MagicKitty <nicolasblin585@gmail.com>
2021-06-27 12:10:29 +0200

    8 min - logout

--- src/app/user/logout/logout.component.ts
+++ src/app/user/logout/logout.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
+  styleUrls: ['./logout.component.css'],
 })
 export class LogoutComponent implements OnInit {
+  userServiceSubscription: Subscription;
+  errorMessage: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private userService: UserService, private router: Router) {
+    this.userServiceSubscription = new Subscription();
+    this.errorMessage = '';
   }
 
+  ngOnInit(): void {}
+
+  logout(): void {
+    this.userService.logout().subscribe(
+      (next) => {
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        this.errorMessage = error;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.userServiceSubscription.unsubscribe();
+  }
 }
