1a6422531c54fb934c92f2ced006939fb931c337
MagicKitty <nicolasblin585@gmail.com>
2021-06-27 14:59:07 +0200

    40min - configuration

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -3,17 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NewComponent } from './new/new.component';
 import { CaseComponent } from './case/case.component';
 import { GameRoutingModule } from './game-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [
-    NewComponent,
-    CaseComponent
-  ],
-  imports: [
-    CommonModule,
-    GameRoutingModule
-  ]
+  declarations: [NewComponent, CaseComponent],
+  imports: [CommonModule, GameRoutingModule, FormsModule, ReactiveFormsModule],
 })
-export class GameModule { }
+export class GameModule {}
--- src/app/game/new/new.component.ts
+++ src/app/game/new/new.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
-  styleUrls: ['./new.component.css']
+  styleUrls: ['./new.component.css'],
 })
 export class NewComponent implements OnInit {
+  table: string[][];
+  tableForm: FormGroup;
+  colsSubscription: Subscription;
+  rowsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.tableForm = this.fb.group({
+      rows: [3, [Validators.required, Validators.min(1)]],
+      cols: [6, [Validators.required, Validators.min(1)]],
+    });
 
-  ngOnInit(): void {
+    this.table = new Array(this.tableForm.controls['rows'].value);
+    for (let i = 0; i < this.tableForm.controls['rows'].value; i++) {
+      this.table[i] = new Array(this.tableForm.controls['cols'].value);
+    }
+
+    this.rowsSubscription = this.tableForm.controls[
+      'rows'
+    ].valueChanges.subscribe((rows) => {
+      this.drawGrid();
+    });
+    this.colsSubscription = this.tableForm.controls[
+      'cols'
+    ].valueChanges.subscribe(() => {
+      this.drawGrid();
+    });
   }
 
+  ngOnInit(): void {}
+
+  drawGrid(): void {
+    this.table = new Array(this.tableForm.controls['rows'].value);
+    for (let i = 0; i < this.tableForm.controls['rows'].value; i++) {
+      this.table[i] = new Array(this.tableForm.controls['cols'].value);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.colsSubscription.unsubscribe();
+    this.rowsSubscription.unsubscribe();
+  }
 }
