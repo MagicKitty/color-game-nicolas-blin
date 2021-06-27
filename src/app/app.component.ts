038d04e533d81f9f49800c9156ee2af1470c554f
MagicKitty <nicolasblin585@gmail.com>
2021-06-27 12:48:00 +0200

    28min - username & login/logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user/models/user';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'color-game';
+  title = 'Color Game';
+  isUserConnect: boolean;
+  userName: string;
+  user$: Observable<User | null>;
+
+  constructor(private userService: UserService) {
+    this.isUserConnect = false;
+    this.userName = '';
+    this.user$ = this.userService.user$;
+  }
 }
